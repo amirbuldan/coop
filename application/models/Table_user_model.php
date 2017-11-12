0a2b64538337202293ea0a6d7e81d3aa3c7124cf
amirbuldan <amirbuldan2915@gmail.com>
2017-11-12 23:53:39 +0700

    delete function join

--- application/models/Table_user_model.php
+++ application/models/Table_user_model.php
@@ -12,18 +12,7 @@ class Table_user_model extends CI_Model {
         else {
             $data = $this->db->get($this->table);
         }
-
         return $data->result();
-    }
-
-    public function get_r_rekening($id_user)
-    {
-        $this->db->select('*')
-            ->from($this->table.' u')
-            ->join('tbl_rekening r', 'u.id_user = r.id_user', 'left')
-            ->where('u.id_user', $id_user);
-        return $this->db->get()
-            ->result();
     }
 
     public function insert($data)
